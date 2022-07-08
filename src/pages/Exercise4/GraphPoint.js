6d129cce4fb7dfc1d511f754d2fee53642e6a773
minhle0105 <minh.anh.le.cnn@gmail.com>
2022-07-08 22:33:12 +0700

    Exercise 4 DONE

--- src/pages/Exercise4/GraphPoint.js
+++ src/pages/Exercise4/GraphPoint.js
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 
 import './styles.css';
 
 export const GraphPoint = React.memo((props) => {
-  const { setGraphPointOn } = props;
-  const [on, setOn] = useState(false);
+    const {setGraphPointOn} = props;
+    const [on, setOn] = useState(false);
 
-  useEffect(
-    function didMount() {
-      setOn(setGraphPointOn());
-    },
-    [setGraphPointOn]
-  );
+    useEffect(
+        function didMount() {
+            setOn(setGraphPointOn());
+        },
+        [setGraphPointOn]
+    );
 
-  return <div className={`e3-graph-point ${on ? 'e3-graph-point-on' : ''}`} />;
+    return <div className={`e3-graph-point ${on ? 'e3-graph-point-on' : ''}`}/>;
 });
